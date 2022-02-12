f2e4d74da34ceb5c22c92698ccf661a08b3bec72
Yamilett Ponce <yamm_260105@hotmail.com>
2022-02-12 12:03:38 -0600

    Chrome Driver

--- SeleniumCurso/src/test/java/com/java/Ejercicios/Decimal.java
+++ SeleniumCurso/src/test/java/com/java/Ejercicios/Decimal.java
@@ -3,13 +3,19 @@ package com.java.Ejercicios;
 public class Decimal {
 
 	public static void main(String[] args) {
-		System.out.println(areEqualByThreeDecimalPlaces(8.908, 8.90));
+		System.out.println(areEqualByThreeDecimalPlaces(8.908, 8.908));
 	}
-	public static boolean areEqualByThreeDecimalPlaces (double x, double y) {
-		int num1 = (int) (x*1000); //Se usa casting para convertir de double a int y se multiplica por 1000 
-		int num2 = (int) (y*1000); // porque se consideran 3 digitos
+
+	public static boolean areEqualByThreeDecimalPlaces(double x, double y) {
+		int num1 = (int) (x * 1000); // Se usa casting para convertir de double a int y se multiplica por 1000
+		int num2 = (int) (y * 1000); // porque se consideran 3 digitos
+
+		// Ternary Operator
+		return (num1 == num2) ? true : false;
 		
-		if (num1==num2) {return true;
-		}else {return false;}
+		// If structure
+		// if (num1==num2) {return true;
+		// }else {return false;}
+
 	}
 }
